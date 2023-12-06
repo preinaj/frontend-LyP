55a766cac9e17ae6407b8169b356238041e34a21
Cricabara0000 <cricabara@alum.us.es>
2023-12-06 17:56:01 +0100

    Branch Created
    Starting to develop frontend in our new branch

---
+++ src/Books.js
@@ -0,0 +1,20 @@
+function Books(props){
+    return{
+        <table className="table">
+            <thead>
+                <tr>
+                    
+                    <th>Título</th>
+                    <th>Autor</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+        </table> 
+    }
+}
+
+export default Books;
